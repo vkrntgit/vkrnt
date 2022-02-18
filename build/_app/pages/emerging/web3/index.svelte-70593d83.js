f22cf0b5da4d30c7a202e3ce53b22073c5a2fc3d
vkrntgit <vikrantpayal@protonmail.com>
2022-02-18 19:06:11 +0530

    built the networking and containerization update

---
+++ build/_app/pages/emerging/web3/index.svelte-70593d83.js
@@ -0,0 +1,107 @@
+import{S as q,i as J,s as K,e as o,t as r,k as U,c as s,a as L,h as a,d as n,m as V,g as i,F as O,G as Y}from"../../../chunks/vendor-3c7d81a0.js";function Q(X){let l,H,p,h,u,b,d,f,m,c,w,y,v,k,W,g,R,T,B,F,C,I,P,S,N,x,G,E,_,j,z,A,D,M;return{c(){l=o("h2"),H=r("Web3"),p=r(`
+What is Web3?`),h=o("br"),u=r(`
+Web3 is a web app architecture that runs apps on blockchains or decentralized networks (cryptoeconomic protocol).
+This is a whole new way of deploying and accessing apps - very different from the server-to-browser protocol of serving apps (web2).
+If there is no server, where is the app hosted? Many 'network participants' host the service. What was done by Cloud providers in web2 is done at a microscopic level by network participants.
+`),b=o("br"),d=o("br"),f=r(`
+
+Why is Web3 important?`),m=o("br"),c=r(`
+Web3 attemps to overcome three main challenges with Web2 apps
+Monetization Challenges Web2 apps rely on user volume growth, selling user data as a primary means of making money.
+Privacy Users have little control on their data or awareness of how it is stored or distributed.
+Security Financial transactions through Web2 apps are under strict government control. Bank accounts or access to finances can be shut down by the ruling authorities at will. This gives the authorities a lever on how technology companies shape their apps and manage user data.
+`),w=o("br"),y=o("br"),v=r(`
+
+Will Web3 ever make it big?
+There is a rising tide of Web3 developers, investors and users
+Performance and rate of adoption of dapps (decentralized apps) is still a major concern
+If Web3 development and investments race ahead of regulations and we get serious value out of future dapps, web3 will be bigger than web2 [opinion]
+It is still highly unlikely that the YouTubes of the world are going away for a couple of decades. Web3 might become an extension of the Internet we know today, not a replacement.[opinion]
+`),k=o("br"),W=o("br"),g=r(`
+
+Reference`),R=o("br"),T=r(`
+Free Code Camp / Nader Dabit`),B=o("br"),F=r(`
+https://tals.substack.com/p/reputation-in-web3`),C=o("br"),I=U(),P=o("br"),S=r(`
+Complete guide / Nader again `),N=o("br"),x=r(`
+A free and in depth masterclass teaching you everything you need to know to build, test, & deploy full stack dapps \u2013 at your own pace with end to end code examples
+
+https://lnkd.in/e7FXpsvP
+
+This guide walks through using and putting together the tools, technologies, and protocols needed for real-world dapp development, including:
+
+The Graph
+Polygon Technology
+IPFS
+Next.js
+Hardhat
+Ethers.js
+
+At around 7,000 words, this is the most in depth tutorial I've written in my career and is, again, writing for myself had I entered the industry today.
+
+This is the fourth post in my web3 series that's now been read over 500,000 times, here are the other three posts in that series:
+
+The Complete Guide to Full Stack Ethereum Development
+https://lnkd.in/gGNNEWR
+
+Building a Full Stack NFT Marketplace on Ethereum with Polygon
+https://lnkd.in/g7539cT
+
+The Complete Guide to Full Stack Solana Development with React, Anchor, Rust, and Phantom
+https://lnkd.in/g94YH57V
+`),G=o("br"),E=r(`
+Great place to start:`),_=o("br"),j=r(`
+https://thirdweb.com/
+`),z=o("br"),A=U(),D=o("br"),M=o("br")},l(e){l=s(e,"H2",{});var t=L(l);H=a(t,"Web3"),t.forEach(n),p=a(e,`
+What is Web3?`),h=s(e,"BR",{}),u=a(e,`
+Web3 is a web app architecture that runs apps on blockchains or decentralized networks (cryptoeconomic protocol).
+This is a whole new way of deploying and accessing apps - very different from the server-to-browser protocol of serving apps (web2).
+If there is no server, where is the app hosted? Many 'network participants' host the service. What was done by Cloud providers in web2 is done at a microscopic level by network participants.
+`),b=s(e,"BR",{}),d=s(e,"BR",{}),f=a(e,`
+
+Why is Web3 important?`),m=s(e,"BR",{}),c=a(e,`
+Web3 attemps to overcome three main challenges with Web2 apps
+Monetization Challenges Web2 apps rely on user volume growth, selling user data as a primary means of making money.
+Privacy Users have little control on their data or awareness of how it is stored or distributed.
+Security Financial transactions through Web2 apps are under strict government control. Bank accounts or access to finances can be shut down by the ruling authorities at will. This gives the authorities a lever on how technology companies shape their apps and manage user data.
+`),w=s(e,"BR",{}),y=s(e,"BR",{}),v=a(e,`
+
+Will Web3 ever make it big?
+There is a rising tide of Web3 developers, investors and users
+Performance and rate of adoption of dapps (decentralized apps) is still a major concern
+If Web3 development and investments race ahead of regulations and we get serious value out of future dapps, web3 will be bigger than web2 [opinion]
+It is still highly unlikely that the YouTubes of the world are going away for a couple of decades. Web3 might become an extension of the Internet we know today, not a replacement.[opinion]
+`),k=s(e,"BR",{}),W=s(e,"BR",{}),g=a(e,`
+
+Reference`),R=s(e,"BR",{}),T=a(e,`
+Free Code Camp / Nader Dabit`),B=s(e,"BR",{}),F=a(e,`
+https://tals.substack.com/p/reputation-in-web3`),C=s(e,"BR",{}),I=V(e),P=s(e,"BR",{}),S=a(e,`
+Complete guide / Nader again `),N=s(e,"BR",{}),x=a(e,`
+A free and in depth masterclass teaching you everything you need to know to build, test, & deploy full stack dapps \u2013 at your own pace with end to end code examples
+
+https://lnkd.in/e7FXpsvP
+
+This guide walks through using and putting together the tools, technologies, and protocols needed for real-world dapp development, including:
+
+The Graph
+Polygon Technology
+IPFS
+Next.js
+Hardhat
+Ethers.js
+
+At around 7,000 words, this is the most in depth tutorial I've written in my career and is, again, writing for myself had I entered the industry today.
+
+This is the fourth post in my web3 series that's now been read over 500,000 times, here are the other three posts in that series:
+
+The Complete Guide to Full Stack Ethereum Development
+https://lnkd.in/gGNNEWR
+
+Building a Full Stack NFT Marketplace on Ethereum with Polygon
+https://lnkd.in/g7539cT
+
+The Complete Guide to Full Stack Solana Development with React, Anchor, Rust, and Phantom
+https://lnkd.in/g94YH57V
+`),G=s(e,"BR",{}),E=a(e,`
+Great place to start:`),_=s(e,"BR",{}),j=a(e,`
+https://thirdweb.com/
+`),z=s(e,"BR",{}),A=V(e),D=s(e,"BR",{}),M=s(e,"BR",{})},m(e,t){i(e,l,t),O(l,H),i(e,p,t),i(e,h,t),i(e,u,t),i(e,b,t),i(e,d,t),i(e,f,t),i(e,m,t),i(e,c,t),i(e,w,t),i(e,y,t),i(e,v,t),i(e,k,t),i(e,W,t),i(e,g,t),i(e,R,t),i(e,T,t),i(e,B,t),i(e,F,t),i(e,C,t),i(e,I,t),i(e,P,t),i(e,S,t),i(e,N,t),i(e,x,t),i(e,G,t),i(e,E,t),i(e,_,t),i(e,j,t),i(e,z,t),i(e,A,t),i(e,D,t),i(e,M,t)},p:Y,i:Y,o:Y,d(e){e&&n(l),e&&n(p),e&&n(h),e&&n(u),e&&n(b),e&&n(d),e&&n(f),e&&n(m),e&&n(c),e&&n(w),e&&n(y),e&&n(v),e&&n(k),e&&n(W),e&&n(g),e&&n(R),e&&n(T),e&&n(B),e&&n(F),e&&n(C),e&&n(I),e&&n(P),e&&n(S),e&&n(N),e&&n(x),e&&n(G),e&&n(E),e&&n(_),e&&n(j),e&&n(z),e&&n(A),e&&n(D),e&&n(M)}}}class $ extends q{constructor(l){super();J(this,l,null,Q,K,{})}}export{$ as default};
---
+++ build/_app/pages/webdev/svelte/index.svelte-3de27445.js
@@ -0,0 +1,61 @@
+import{S as xt,i as Ht,s as Kt,H as Vt,e as s,k as u,t as o,c as f,m as b,a as R,h as r,d as e,b as _t,g as i,F as y,I as $t,J as jt,K as Ct,q as qt,o as Dt}from"../../../chunks/vendor-3c7d81a0.js";function Gt(B){let m,S,k,n,c,E,x,H,K,V,$,j,d,at,C,q,D,v,wt,G,T,J,M,O,P,_,Rt,U,a,yt,A,w,Bt,F,L,N,z,Q,W,X,Y,Z,h,g,tt,lt,et,it,st,ft,ot,rt,pt,ut,bt,nt,mt,vt,I;const St=B[1].default,p=Vt(St,B,B[0],null);return{c(){m=s("br"),S=s("br"),k=u(),n=s("h2"),c=o("Svelte + Svelte Kit notes"),E=o(`
+
+I picked Svelte Kit in Jan 2022. Great framework to build node apps. I liked it so much that I moved this site from plain html+css site to Sveltei Kit. The framework is SEO friendly, pre-renders pages and allows rapid development of components and static pages. `),x=s("br"),H=u(),K=s("br"),V=u(),$=s("br"),j=u(),d=s("h3"),at=o("Here are a few basic instructions to get started on Svelte Kit"),C=o(`
+
+
+
+How to start a Svelte project`),q=s("br"),D=u(),v=s("div"),wt=o("npm init svelte@next "),G=s("name"),T=u(),J=s("br"),M=o(`
+How to build and deploy`),O=s("br"),P=u(),_=s("div"),Rt=o("npm install"),U=u(),a=s("div"),yt=o("npm run build"),A=u(),w=s("div"),Bt=o("npm run dev&"),F=o(` (to run npm in backgroud)
+`),L=s("br"),N=s("br"),z=o(`
+
+In case you run without \u2018&\u2019 (in foreground), use right Ctrl and \u2018c\u2019 to terminate server and return to terminal prompt
+`),Q=s("br"),W=s("br"),X=o(`
+Copy and push public folder to deploy to production`),Y=s("br"),Z=s("br"),h=u(),g=s("br"),tt=s("br"),lt=o(`
+
+
+Save layout to src/routes/__layout.svelte
+This file should have the header, main and footer tags with elements common to all pages.
+Use `),p&&p.c(),et=o(` for content specific to each page.
+In the individual pages, you don\u2019t need to code the common elements. Jump directly to components and text specific to that page.
+
+\u2028Save css to src/styles/global.css
+`),it=s("br"),st=s("br"),ft=o(`
+
+Good training on basics:`),ot=s("br"),rt=o(`
+https://www.youtube.com/watch?v=ftiTVitDbx0&list=PL4cUxeGkcC9hpM9ARM59Ve3jqcb54dqiP&index=4
+`),pt=s("br"),ut=s("br"),bt=o(`
+
+Other references:
+https://joshcollinsworth.com/blog/build-static-sveltekit-markdown-blog
+09 Feb 2022
+`),nt=s("br"),mt=s("br"),vt=s("br"),this.h()},l(t){m=f(t,"BR",{}),S=f(t,"BR",{}),k=b(t),n=f(t,"H2",{});var l=R(n);c=r(l,"Svelte + Svelte Kit notes"),l.forEach(e),E=r(t,`
+
+I picked Svelte Kit in Jan 2022. Great framework to build node apps. I liked it so much that I moved this site from plain html+css site to Sveltei Kit. The framework is SEO friendly, pre-renders pages and allows rapid development of components and static pages. `),x=f(t,"BR",{}),H=b(t),K=f(t,"BR",{}),V=b(t),$=f(t,"BR",{}),j=b(t),d=f(t,"H3",{});var ct=R(d);at=r(ct,"Here are a few basic instructions to get started on Svelte Kit"),ct.forEach(e),C=r(t,`
+
+
+
+How to start a Svelte project`),q=f(t,"BR",{}),D=b(t),v=f(t,"DIV",{class:!0});var kt=R(v);wt=r(kt,"npm init svelte@next "),G=f(kt,"NAME",{}),R(G).forEach(e),kt.forEach(e),T=b(t),J=f(t,"BR",{}),M=r(t,`
+How to build and deploy`),O=f(t,"BR",{}),P=b(t),_=f(t,"DIV",{class:!0});var dt=R(_);Rt=r(dt,"npm install"),dt.forEach(e),U=b(t),a=f(t,"DIV",{class:!0});var It=R(a);yt=r(It,"npm run build"),It.forEach(e),A=b(t),w=f(t,"DIV",{class:!0});var Et=R(w);Bt=r(Et,"npm run dev&"),Et.forEach(e),F=r(t,` (to run npm in backgroud)
+`),L=f(t,"BR",{}),N=f(t,"BR",{}),z=r(t,`
+
+In case you run without \u2018&\u2019 (in foreground), use right Ctrl and \u2018c\u2019 to terminate server and return to terminal prompt
+`),Q=f(t,"BR",{}),W=f(t,"BR",{}),X=r(t,`
+Copy and push public folder to deploy to production`),Y=f(t,"BR",{}),Z=f(t,"BR",{}),h=b(t),g=f(t,"BR",{}),tt=f(t,"BR",{}),lt=r(t,`
+
+
+Save layout to src/routes/__layout.svelte
+This file should have the header, main and footer tags with elements common to all pages.
+Use `),p&&p.l(t),et=r(t,` for content specific to each page.
+In the individual pages, you don\u2019t need to code the common elements. Jump directly to components and text specific to that page.
+
+\u2028Save css to src/styles/global.css
+`),it=f(t,"BR",{}),st=f(t,"BR",{}),ft=r(t,`
+
+Good training on basics:`),ot=f(t,"BR",{}),rt=r(t,`
+https://www.youtube.com/watch?v=ftiTVitDbx0&list=PL4cUxeGkcC9hpM9ARM59Ve3jqcb54dqiP&index=4
+`),pt=f(t,"BR",{}),ut=f(t,"BR",{}),bt=r(t,`
+
+Other references:
+https://joshcollinsworth.com/blog/build-static-sveltekit-markdown-blog
+09 Feb 2022
+`),nt=f(t,"BR",{}),mt=f(t,"BR",{}),vt=f(t,"BR",{}),this.h()},h(){_t(v,"class","code"),_t(_,"class","code"),_t(a,"class","code"),_t(w,"class","code")},m(t,l){i(t,m,l),i(t,S,l),i(t,k,l),i(t,n,l),y(n,c),i(t,E,l),i(t,x,l),i(t,H,l),i(t,K,l),i(t,V,l),i(t,$,l),i(t,j,l),i(t,d,l),y(d,at),i(t,C,l),i(t,q,l),i(t,D,l),i(t,v,l),y(v,wt),y(v,G),i(t,T,l),i(t,J,l),i(t,M,l),i(t,O,l),i(t,P,l),i(t,_,l),y(_,Rt),i(t,U,l),i(t,a,l),y(a,yt),i(t,A,l),i(t,w,l),y(w,Bt),i(t,F,l),i(t,L,l),i(t,N,l),i(t,z,l),i(t,Q,l),i(t,W,l),i(t,X,l),i(t,Y,l),i(t,Z,l),i(t,h,l),i(t,g,l),i(t,tt,l),i(t,lt,l),p&&p.m(t,l),i(t,et,l),i(t,it,l),i(t,st,l),i(t,ft,l),i(t,ot,l),i(t,rt,l),i(t,pt,l),i(t,ut,l),i(t,bt,l),i(t,nt,l),i(t,mt,l),i(t,vt,l),I=!0},p(t,[l]){p&&p.p&&(!I||l&1)&&$t(p,St,t,t[0],I?Ct(St,t[0],l,null):jt(t[0]),null)},i(t){I||(qt(p,t),I=!0)},o(t){Dt(p,t),I=!1},d(t){t&&e(m),t&&e(S),t&&e(k),t&&e(n),t&&e(E),t&&e(x),t&&e(H),t&&e(K),t&&e(V),t&&e($),t&&e(j),t&&e(d),t&&e(C),t&&e(q),t&&e(D),t&&e(v),t&&e(T),t&&e(J),t&&e(M),t&&e(O),t&&e(P),t&&e(_),t&&e(U),t&&e(a),t&&e(A),t&&e(w),t&&e(F),t&&e(L),t&&e(N),t&&e(z),t&&e(Q),t&&e(W),t&&e(X),t&&e(Y),t&&e(Z),t&&e(h),t&&e(g),t&&e(tt),t&&e(lt),p&&p.d(t),t&&e(et),t&&e(it),t&&e(st),t&&e(ft),t&&e(ot),t&&e(rt),t&&e(pt),t&&e(ut),t&&e(bt),t&&e(nt),t&&e(mt),t&&e(vt)}}}function Tt(B,m,S){let{$$slots:k={},$$scope:n}=m;return B.$$set=c=>{"$$scope"in c&&S(0,n=c.$$scope)},[n,k]}class Mt extends xt{constructor(m){super();Ht(this,m,Tt,Gt,Kt,{})}}export{Mt as default};
