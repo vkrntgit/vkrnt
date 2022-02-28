6f0cc824bfe3554bd0b5e62b1c3c419dca628eba
vkrntgit <vikrantpayal@protonmail.com>
2022-02-28 21:21:42 +0530

    added natting details in networking page

--- src/routes/infrastructure/networking/concepts/index.svelte
+++ src/routes/infrastructure/networking/concepts/index.svelte
@@ -4,3 +4,7 @@
 <h3> 301 and 302 redirects</h3>
 A 301 redirect is a status code that tells users and search engine that the page has moved permanently. A 302 redirect is used for temporary redirects. More <a href="https://www.domain.com/blog/what-is-a-redirect/">here</a>.
 <br><br>
+<h3>NAT'ing (NATting) </h3>
+NAT stands for network address translation. This is the process where you use a firewall to assign a public IP to a computer inside a private network. Helps limit the number of public IP addresses an organization uses by allowing private IP networks to use unregistered IP addresses, but then converting to globally unique.<br>
+<br>
+
