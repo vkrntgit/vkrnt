01ddffb7299e62495a690c0a0fa85d453b36c208
vkrntgit <vikrantpayal@protonmail.com>
2022-03-19 11:31:41 +0530

    home page changes, added table

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,44 +4,90 @@
 
 <main>
 <br><br>
-<h2>Blog posts</h2>
-<ul>
-    <li> 
-        <a href="/blog/learning-is-a-fractal-curve">Learning is a fractal curve</a>
-        <div class="code">#culture</div>
-    </li>
-
-</ul>
+<h2>&#128240; &nbsp; Blog</h2> <!-- &#128452 is the unicode for a closed drawer --> <!-- source of unicodes http://xahlee.info/comp/unicode_office_icons.html -->
+ <a href="/blog/learning-is-a-fractal-curve">Learning is a fractal curve</a>
+<br><br><br><br>
+<h2>&#128452; &nbsp;Technology Slipbox</h2> My notes on development, CICD and infrastructure technologies and tools.
 <br><br>
-<h2>Technology Slipbox</h2> Notes on development, CICD and infrastructure technologies and tools.
-<br><br><ul>
-    
-              <li> <h3>Cloud</h3> </li>
-              <li> Linode - Great place to playaround. Uncomplicated. Inexpensive. </li>
-              <li> <a href="/infrastructure/cloud/cleo"> Cleo </a> &nbsp; cross platform integration. </li>
-<br>
-              <li> <h3> Containerization </h3> </li>
-              <li> <a href="/infrastructure/containerization/docker"> Docker</a> </li>
-              <li> <a href="/infrastructure/containerization/containerd"> Containerd</a> </li>
-              <li> <a href="/infrastructure/containerization/cri.o"> CRI.O</a></li>
-<br>
-              <li> <h3> Telemetry </h3> </li>
-              <li> <a href="/telemetry/apache_skywalking"> Apache Skywalking </a> </li>
-<br>
-              <li> <h3> Networking </h3> </li>
+
+<table class="slipbox">
+    <tr class="drawer">
+         <td class="drawer_label">
+             CLOUD
+         </td>   
+         <td>     
+                      <a href="/infrastructure/cloud/linode">Linode</a> - Great as a sandbox to learn cloud basics, GCP,        
+                       <a href="/infrastructure/cloud/cleo"> Cleo </a> &nbsp; cross platform integration. 
+         </td>
+    </tr>
+
+    <tr class="drawer">
+         <td class="drawer_label">
+              KUBERNETES
+         </td>
+         <td>
+         </td>
+    </tr>
+     <tr>
+         <td class="drawer_label">
+	     CONTAINERISATION
+         </td>
+          <td>
+
+               <a href="/infrastructure/containerization/docker"> Docker</a>,  
+               <a href="/infrastructure/containerization/containerd"> Containerd</a>,  
+               <a href="/infrastructure/containerization/cri.o"> CRI.O</a>
+         </td>
+     </tr>
+
+
+     <tr>
+         <td class="drawer_label">
+            TELEMETRY
+         </td>
+         <td>
+
+               <a href="/telemetry/apache_skywalking"> Apache Skywalking </a> 
+         </td>
+     </tr>
+
+     <tr>
+         <td class="drawer_label"> 
+            NETWORKING
+         </td> 
+         <td> 
+         
               <li> <a href="/infrastructure/networking/concepts/"> Networking concepts</a> </li>
-<br>
-              <li> <h3> Development languages </h3> </li>
-              <li> <a href="/development/go"> Go lang </a> </li>
-<br>
-              <li> <h3>Webdev</h3> </li> 
-              <li> <a href="/webdev/svelte">Svelte + Sveltkit</a> &nbsp; A great framework for rapid web app development!</li>
-<br>
-              <li> <h3>Emerging Tech</h3> </li>
-              <li> <a href="/emerging/web3"> Web3 </a>  &nbsp; Decentralized, blockchain based web development. Scary yet interesting. 
+         
+         </td> 
+    </tr>
+
+    <tr>
+        <td class="drawer_label"> 
+             DEVELOPMENT
+        </td> 
+        <td>
+         
+               <a href="/development/go"> Go lang </a>,  
+               <a href="/webdev/svelte">Svelte + Sveltkit</a> &nbsp; A great framework for rapid web app development!
+        <td>
+   </tr>
+
+    <tr>
+        <td class="drawer_label">
+             EMERGING TECH
+        </td>
+        <td>
+            <a href="/emerging/web3">Web3</a> Decentralized block chain based development. Scary yet interesting.
+        <td>
+    </tr>
+</table>
+
+<br><br><br>
+
+
        
 
-        </ul>
 <br><br>
 
 </main>
