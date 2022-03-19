49835b362598913f8237b25e54bfec1c24db43b6
vkrntgit <vikrantpayal@protonmail.com>
2022-03-19 20:07:33 +0530

    added dark mode

--- src/lib/title.svelte
+++ src/lib/title.svelte
@@ -1,6 +1,20 @@
 <script>
     export let title = "vkrnt.com"
     import '../style/global.css'
+
+	function darkMode(){
+		var element = document.body;
+		element.className = "dark-mode";
+                document.getElementById("dark_mode_button").style.display="none";
+                document.getElementById("light_mode_button").style.display= "inline";
+	}
+	function lightMode() {
+		var element = document.body;
+		element.className = "light-mode";
+                document.getElementById("light_mode_button").style.display="none";
+                document.getElementById("dark_mode_button").style.display= "inline";
+	}
+
 </script>
 
 
@@ -11,9 +25,14 @@
               <td  id="logo">   {title}   </td> 
               <td class="header_table_td"> <a href="/">   Home   </a></td> 
               <td class="header_table_td"> <a href="/about">   About   </a></td> 
-          </tr> 
+              <td class="brightness"> 
+                      <span id="dark_mode_button" on:click="{darkMode}" title="dark mode"> &#128526; </span>
+                      <span id="light_mode_button" on:click="{lightMode}" title="light mode"> &#129299; </span>
+              </td> 
+         </tr> 
        </table>
-          
+         
+ 
 </div>
 
 <style>
@@ -27,4 +46,31 @@
         border-style: none;
         padding: 50px;
     }
+
+   #light_mode_button{
+      display: none;
+      cursor: pointer;
+   }
+
+  #light_mode_button:hover{
+     font-size: 160%;
+  }
+
+  #dark_mode_button{
+     cursor: pointer;
+  }
+
+  #dark_mode_button:hover {
+     font-size: 160%;
+  }
+  
 </style>
+
+
+
+
+
+
+
+
+
