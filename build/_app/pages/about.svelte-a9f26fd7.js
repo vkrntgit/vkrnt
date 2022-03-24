974d709e3d1b39cc660dfabf486629d83ea65078
vkrntgit <vikrantpayal@protonmail.com>
2022-03-24 23:08:08 +0530

    blog fractal2

---
+++ build/_app/pages/about.svelte-a9f26fd7.js
@@ -0,0 +1,13 @@
+import{S as Z,i as $,s as tt,e as a,k as h,t as n,c as r,a as m,m as c,h as i,d as u,b as d,N as et,g as st,F as s,H as O}from"../chunks/vendor-62c3b85b.js";function at(X){let t,v,_,f,w,b,B,R,g,y,I,k,T,x,E,P,S,V,j,C,q,A,D,G,M,z,l,F,o,Q,H,N,J,K,L;return{c(){t=a("div"),v=a("br"),_=h(),f=a("br"),w=n(`
+I `),b=a("red"),B=n("\u2665"),R=n(" tech."),g=a("br"),y=h(),I=a("br"),k=n(`
+This website is a small side project to research and share the latest in technology, leadership in IT and the application of technology to real world business problems.`),T=a("br"),x=h(),E=a("br"),P=n(`
+I share my thoughts in the Blog section. The Slipbox is a collection of notes on technologies that I have worked on and the ones I am exploring. `),S=a("br"),V=a("br"),j=n(`
+
+This site was developed with sveltekit and is hosted on a GCP VM. `),C=a("br"),q=a("br"),A=n(`
+Vikrant Payal`),D=a("br"),G=h(),M=a("br"),z=h(),l=a("a"),F=n("Tweet to @vikrantpayal"),o=a("script"),H=h(),N=a("br"),J=h(),K=a("br"),L=a("br"),this.h()},l(p){t=r(p,"DIV",{class:!0});var e=m(t);v=r(e,"BR",{}),_=c(e),f=r(e,"BR",{}),w=i(e,`
+I `),b=r(e,"RED",{});var U=m(b);B=i(U,"\u2665"),U.forEach(u),R=i(e," tech."),g=r(e,"BR",{}),y=c(e),I=r(e,"BR",{}),k=i(e,`
+This website is a small side project to research and share the latest in technology, leadership in IT and the application of technology to real world business problems.`),T=r(e,"BR",{}),x=c(e),E=r(e,"BR",{}),P=i(e,`
+I share my thoughts in the Blog section. The Slipbox is a collection of notes on technologies that I have worked on and the ones I am exploring. `),S=r(e,"BR",{}),V=r(e,"BR",{}),j=i(e,`
+
+This site was developed with sveltekit and is hosted on a GCP VM. `),C=r(e,"BR",{}),q=r(e,"BR",{}),A=i(e,`
+Vikrant Payal`),D=r(e,"BR",{}),G=c(e),M=r(e,"BR",{}),z=c(e),l=r(e,"A",{href:!0,class:!0,"data-show-count":!0});var W=m(l);F=i(W,"Tweet to @vikrantpayal"),W.forEach(u),o=r(e,"SCRIPT",{src:!0,charset:!0});var Y=m(o);Y.forEach(u),H=c(e),N=r(e,"BR",{}),J=c(e),K=r(e,"BR",{}),L=r(e,"BR",{}),e.forEach(u),this.h()},h(){d(l,"href","https://twitter.com/intent/tweet?screen_name=vikrantpayal&ref_src=twsrc%5Etfw"),d(l,"class","twitter-mention-button"),d(l,"data-show-count","false"),o.async=!0,et(o.src,Q="https://platform.twitter.com/widgets.js")||d(o,"src",Q),d(o,"charset","utf-8"),d(t,"class","article svelte-2itvzt")},m(p,e){st(p,t,e),s(t,v),s(t,_),s(t,f),s(t,w),s(t,b),s(b,B),s(t,R),s(t,g),s(t,y),s(t,I),s(t,k),s(t,T),s(t,x),s(t,E),s(t,P),s(t,S),s(t,V),s(t,j),s(t,C),s(t,q),s(t,A),s(t,D),s(t,G),s(t,M),s(t,z),s(t,l),s(l,F),s(t,o),s(t,H),s(t,N),s(t,J),s(t,K),s(t,L)},p:O,i:O,o:O,d(p){p&&u(t)}}}class lt extends Z{constructor(t){super();$(this,t,null,at,tt,{})}}export{lt as default};
