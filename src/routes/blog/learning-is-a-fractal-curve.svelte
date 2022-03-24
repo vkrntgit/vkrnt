819ce5da132d803be99648ae82fbe075daecab46
vkrntgit <vikrantpayal@protonmail.com>
2022-03-24 23:06:52 +0530

    blog fractal

--- src/routes/blog/learning-is-a-fractal-curve.svelte
+++ src/routes/blog/learning-is-a-fractal-curve.svelte
@@ -2,6 +2,14 @@
 </script>
 <div class="article">
 <h2>Learning is a fractal curve</h2>
+<br><br>
+How you treat a small lesson in any technology decides how you evolve in that area over the years. 
+<br><br>
+Everyone in tech start at almost the same line. Some race ahead. Yet some turn into SMEs. We all have the same 24 hours in the day. Some make more out of it than others. I think the differentiator is how we exploit each hour. Learning new tech - fast - gives you a huge advantage. May prefer the approach of courses and certification. There is nothing wrong with leaning on courses. Certifications are a fantastic way to show the world what you know. Beyond these objectives, when it comes to <i>really</i> getting a handle of something, hands-on practice is the unbeatable champion. 
+<br><br>
+Learning this way takes longer though. Learning a completely new platform/tool/tech can be daunting. Coffee can only carry you for a couple of hours. An easy way to learn consistently and grow in a new area is to break down the subject matter into bite size lessons. There is a significant qualititative difference in understanding and ability to apply knowledge when you focus all your attention on a ten minute information nugget - as against racing through a hundred pages or watching a web course at 2x speed. The best measure of success is how easily you move from learning to application. 
+<br><br>
+The jump from learning to application is tougher when you absorb a ten hour course or complete a three hundred page book over a weekend. 
 </div>
 <style>
 </style>
