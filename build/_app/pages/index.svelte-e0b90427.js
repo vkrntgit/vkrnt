b8363a23d8b1c26b187f59ef00932882a490154e
vkrntgit <vikrantpayal@protonmail.com>
2022-04-07 05:44:26 +0530

    added hardware, removed blank links

---
+++ build/_app/pages/index.svelte-e0b90427.js
@@ -0,0 +1,49 @@
+import{S as Sr,i as wr,s as Cr,e,k as c,t as s,c as r,a as n,m as b,h as d,d as a,b as f,g as Ir,F as t,H as lr}from"../chunks/vendor-62c3b85b.js";function Gr(mr){let l,Rt,it,vt,_t,Tt,V,ut,Dt,I,gt,Bt,pt,At,Nt,mt,St,wt,Ct,It,Gt,kt,Mt,yt,Ot,Lt,Y,xt,Kt,Ht,Pt,Wt,zt,E,T,G,Ut,Ft,R,Qt,Vt,Yt,jt,qt,Jt,Xt,Zt,$t,u,k,te,ee,D,re,le,ae,ne,oe,A,M,se,de,j,ce,be,N,y,Ee,he,i,fe,Re,ie,ve,_e,Te,ue,De,ge,m,O,Be,pe,q,Ae,Ne,S,L,me,Se,J,x,we,Ce,g,K,Ie,Ge,B,H,ke,Me,P,ye,Oe,at,Le,w,W,xe,Ke,X,He,Pe,C,z,We,ze,Z,Ue,Fe,p,U,Qe,Ve,F,Q,Ye,je,nt,qe,Je,Xe,Ze,$e,tr,er;return{c(){l=e("main"),Rt=e("hr"),it=c(),vt=e("br"),_t=e("br"),Tt=c(),V=e("h2"),ut=s("\u{1F4F0} \xA0 Blog"),Dt=c(),I=e("a"),gt=s("Learning is a fractal curve"),Bt=c(),pt=e("br"),At=c(),Nt=e("br"),mt=c(),St=e("br"),wt=c(),Ct=e("br"),It=c(),Gt=e("hr"),kt=c(),Mt=e("br"),yt=c(),Ot=e("br"),Lt=c(),Y=e("h2"),xt=s("\u{1F5C4} \xA0Technology Slipbox"),Kt=s(`
+  My notes on development, CICD and infrastructure technologies and tools.
+  `),Ht=e("br"),Pt=c(),Wt=e("br"),zt=c(),E=e("table"),T=e("tr"),G=e("td"),Ut=s("CLOUD"),Ft=c(),R=e("td"),Qt=s(`Linode- Great as a sandbox to learn cloud basics
+        `),Vt=e("br"),Yt=s(`
+        GCP
+        `),jt=e("br"),qt=s(`
+        AWS
+        `),Jt=e("br"),Xt=s(`
+        Azure
+        `),Zt=e("br"),$t=c(),u=e("tr"),k=e("td"),te=s("KUBERNETES"),ee=c(),D=e("td"),re=s(`RKE
+        `),le=e("br"),ae=s(`
+        GKE
+        `),ne=e("br"),oe=c(),A=e("tr"),M=e("td"),se=s("CONTAINERISATION"),de=c(),j=e("td"),ce=s("Docker, Containerd, CRI.O"),be=c(),N=e("tr"),y=e("td"),Ee=s("DATABASES"),he=c(),i=e("td"),fe=s(`MongoDb
+        `),Re=e("br"),ie=s(`
+        Postgres
+        `),ve=e("br"),_e=s(`
+        MySQL
+        `),Te=e("br"),ue=s(`
+        Couchebase
+        `),De=e("br"),ge=c(),m=e("tr"),O=e("td"),Be=s("TELEMETRY"),pe=c(),q=e("td"),Ae=s("Apache Skywalking"),Ne=c(),S=e("tr"),L=e("td"),me=s("NETWORKING"),Se=c(),J=e("td"),x=e("a"),we=s("Networking concepts"),Ce=c(),g=e("tr"),K=e("td"),Ie=s("DEVELOPMENT"),Ge=c(),B=e("td"),H=e("a"),ke=s("Go lang "),Me=s(`,
+        `),P=e("a"),ye=s("Svelte + Sveltkit"),Oe=s(` \xA0 A great framework for
+        rapid web app development!
+      `),at=e("td"),Le=c(),w=e("tr"),W=e("td"),xe=s("PERFORMANCE ENGINEERING"),Ke=c(),X=e("td"),He=s("K6 - load testing tool"),Pe=c(),C=e("tr"),z=e("td"),We=s("HARDWARE"),ze=c(),Z=e("td"),Ue=s("Cores"),Fe=c(),p=e("tr"),U=e("td"),Qe=s("EMERGING TECH"),Ve=c(),F=e("td"),Q=e("a"),Ye=s("Web3"),je=s(` Decentralized block chain based development.
+        Scary yet interesting.
+      `),nt=e("td"),qe=c(),Je=e("br"),Xe=e("br"),Ze=e("br"),$e=c(),tr=e("br"),er=e("br"),this.h()},l($){l=r($,"MAIN",{});var o=n(l);Rt=r(o,"HR",{}),it=b(o),vt=r(o,"BR",{}),_t=r(o,"BR",{}),Tt=b(o),V=r(o,"H2",{});var ar=n(V);ut=d(ar,"\u{1F4F0} \xA0 Blog"),ar.forEach(a),Dt=b(o),I=r(o,"A",{href:!0});var nr=n(I);gt=d(nr,"Learning is a fractal curve"),nr.forEach(a),Bt=b(o),pt=r(o,"BR",{}),At=b(o),Nt=r(o,"BR",{}),mt=b(o),St=r(o,"BR",{}),wt=b(o),Ct=r(o,"BR",{}),It=b(o),Gt=r(o,"HR",{}),kt=b(o),Mt=r(o,"BR",{}),yt=b(o),Ot=r(o,"BR",{}),Lt=b(o),Y=r(o,"H2",{});var or=n(Y);xt=d(or,"\u{1F5C4} \xA0Technology Slipbox"),or.forEach(a),Kt=d(o,`
+  My notes on development, CICD and infrastructure technologies and tools.
+  `),Ht=r(o,"BR",{}),Pt=b(o),Wt=r(o,"BR",{}),zt=b(o),E=r(o,"TABLE",{class:!0});var h=n(E);T=r(h,"TR",{class:!0});var ot=n(T);G=r(ot,"TD",{class:!0});var sr=n(G);Ut=d(sr,"CLOUD"),sr.forEach(a),Ft=b(ot),R=r(ot,"TD",{});var v=n(R);Qt=d(v,`Linode- Great as a sandbox to learn cloud basics
+        `),Vt=r(v,"BR",{}),Yt=d(v,`
+        GCP
+        `),jt=r(v,"BR",{}),qt=d(v,`
+        AWS
+        `),Jt=r(v,"BR",{}),Xt=d(v,`
+        Azure
+        `),Zt=r(v,"BR",{}),v.forEach(a),ot.forEach(a),$t=b(h),u=r(h,"TR",{class:!0});var st=n(u);k=r(st,"TD",{class:!0});var dr=n(k);te=d(dr,"KUBERNETES"),dr.forEach(a),ee=b(st),D=r(st,"TD",{});var tt=n(D);re=d(tt,`RKE
+        `),le=r(tt,"BR",{}),ae=d(tt,`
+        GKE
+        `),ne=r(tt,"BR",{}),tt.forEach(a),st.forEach(a),oe=b(h),A=r(h,"TR",{});var dt=n(A);M=r(dt,"TD",{class:!0});var cr=n(M);se=d(cr,"CONTAINERISATION"),cr.forEach(a),de=b(dt),j=r(dt,"TD",{});var br=n(j);ce=d(br,"Docker, Containerd, CRI.O"),br.forEach(a),dt.forEach(a),be=b(h),N=r(h,"TR",{});var ct=n(N);y=r(ct,"TD",{class:!0});var Er=n(y);Ee=d(Er,"DATABASES"),Er.forEach(a),he=b(ct),i=r(ct,"TD",{});var _=n(i);fe=d(_,`MongoDb
+        `),Re=r(_,"BR",{}),ie=d(_,`
+        Postgres
+        `),ve=r(_,"BR",{}),_e=d(_,`
+        MySQL
+        `),Te=r(_,"BR",{}),ue=d(_,`
+        Couchebase
+        `),De=r(_,"BR",{}),_.forEach(a),ct.forEach(a),ge=b(h),m=r(h,"TR",{});var bt=n(m);O=r(bt,"TD",{class:!0});var hr=n(O);Be=d(hr,"TELEMETRY"),hr.forEach(a),pe=b(bt),q=r(bt,"TD",{});var fr=n(q);Ae=d(fr,"Apache Skywalking"),fr.forEach(a),bt.forEach(a),Ne=b(h),S=r(h,"TR",{});var Et=n(S);L=r(Et,"TD",{class:!0});var Rr=n(L);me=d(Rr,"NETWORKING"),Rr.forEach(a),Se=b(Et),J=r(Et,"TD",{});var ir=n(J);x=r(ir,"A",{href:!0});var vr=n(x);we=d(vr,"Networking concepts"),vr.forEach(a),ir.forEach(a),Et.forEach(a),Ce=b(h),g=r(h,"TR",{});var et=n(g);K=r(et,"TD",{class:!0});var _r=n(K);Ie=d(_r,"DEVELOPMENT"),_r.forEach(a),Ge=b(et),B=r(et,"TD",{});var rt=n(B);H=r(rt,"A",{href:!0});var Tr=n(H);ke=d(Tr,"Go lang "),Tr.forEach(a),Me=d(rt,`,
+        `),P=r(rt,"A",{href:!0});var ur=n(P);ye=d(ur,"Svelte + Sveltkit"),ur.forEach(a),Oe=d(rt,` \xA0 A great framework for
+        rapid web app development!
+      `),rt.forEach(a),at=r(et,"TD",{}),n(at).forEach(a),et.forEach(a),Le=b(h),w=r(h,"TR",{});var ht=n(w);W=r(ht,"TD",{class:!0});var Dr=n(W);xe=d(Dr,"PERFORMANCE ENGINEERING"),Dr.forEach(a),Ke=b(ht),X=r(ht,"TD",{});var gr=n(X);He=d(gr,"K6 - load testing tool"),gr.forEach(a),ht.forEach(a),Pe=b(h),C=r(h,"TR",{});var ft=n(C);z=r(ft,"TD",{class:!0});var Br=n(z);We=d(Br,"HARDWARE"),Br.forEach(a),ze=b(ft),Z=r(ft,"TD",{});var pr=n(Z);Ue=d(pr,"Cores"),pr.forEach(a),ft.forEach(a),Fe=b(h),p=r(h,"TR",{});var lt=n(p);U=r(lt,"TD",{class:!0});var Ar=n(U);Qe=d(Ar,"EMERGING TECH"),Ar.forEach(a),Ve=b(lt),F=r(lt,"TD",{});var rr=n(F);Q=r(rr,"A",{href:!0});var Nr=n(Q);Ye=d(Nr,"Web3"),Nr.forEach(a),je=d(rr,` Decentralized block chain based development.
+        Scary yet interesting.
+      `),rr.forEach(a),nt=r(lt,"TD",{}),n(nt).forEach(a),lt.forEach(a),h.forEach(a),qe=b(o),Je=r(o,"BR",{}),Xe=r(o,"BR",{}),Ze=r(o,"BR",{}),$e=b(o),tr=r(o,"BR",{}),er=r(o,"BR",{}),o.forEach(a),this.h()},h(){f(I,"href","/blog/learning-is-a-fractal-curve"),f(G,"class","drawer_label"),f(T,"class","drawer"),f(k,"class","drawer_label"),f(u,"class","drawer"),f(M,"class","drawer_label"),f(y,"class","drawer_label"),f(O,"class","drawer_label"),f(L,"class","drawer_label"),f(x,"href","/infrastructure/networking/concepts/"),f(K,"class","drawer_label"),f(H,"href","/development/go"),f(P,"href","/webdev/svelte"),f(W,"class","drawer_label"),f(z,"class","drawer_label"),f(U,"class","drawer_label"),f(Q,"href","/emerging/web3"),f(E,"class","slipbox")},m($,o){Ir($,l,o),t(l,Rt),t(l,it),t(l,vt),t(l,_t),t(l,Tt),t(l,V),t(V,ut),t(l,Dt),t(l,I),t(I,gt),t(l,Bt),t(l,pt),t(l,At),t(l,Nt),t(l,mt),t(l,St),t(l,wt),t(l,Ct),t(l,It),t(l,Gt),t(l,kt),t(l,Mt),t(l,yt),t(l,Ot),t(l,Lt),t(l,Y),t(Y,xt),t(l,Kt),t(l,Ht),t(l,Pt),t(l,Wt),t(l,zt),t(l,E),t(E,T),t(T,G),t(G,Ut),t(T,Ft),t(T,R),t(R,Qt),t(R,Vt),t(R,Yt),t(R,jt),t(R,qt),t(R,Jt),t(R,Xt),t(R,Zt),t(E,$t),t(E,u),t(u,k),t(k,te),t(u,ee),t(u,D),t(D,re),t(D,le),t(D,ae),t(D,ne),t(E,oe),t(E,A),t(A,M),t(M,se),t(A,de),t(A,j),t(j,ce),t(E,be),t(E,N),t(N,y),t(y,Ee),t(N,he),t(N,i),t(i,fe),t(i,Re),t(i,ie),t(i,ve),t(i,_e),t(i,Te),t(i,ue),t(i,De),t(E,ge),t(E,m),t(m,O),t(O,Be),t(m,pe),t(m,q),t(q,Ae),t(E,Ne),t(E,S),t(S,L),t(L,me),t(S,Se),t(S,J),t(J,x),t(x,we),t(E,Ce),t(E,g),t(g,K),t(K,Ie),t(g,Ge),t(g,B),t(B,H),t(H,ke),t(B,Me),t(B,P),t(P,ye),t(B,Oe),t(g,at),t(E,Le),t(E,w),t(w,W),t(W,xe),t(w,Ke),t(w,X),t(X,He),t(E,Pe),t(E,C),t(C,z),t(z,We),t(C,ze),t(C,Z),t(Z,Ue),t(E,Fe),t(E,p),t(p,U),t(U,Qe),t(p,Ve),t(p,F),t(F,Q),t(Q,Ye),t(F,je),t(p,nt),t(l,qe),t(l,Je),t(l,Xe),t(l,Ze),t(l,$e),t(l,tr),t(l,er)},p:lr,i:lr,o:lr,d($){$&&a(l)}}}class Mr extends Sr{constructor(l){super();wr(this,l,null,Gr,Cr,{})}}export{Mr as default};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,137 +1,110 @@
 <script>
 </script>
 
-
 <main>
-<hr>
-<br><br>
-<h2>&#128240; &nbsp; Blog</h2> <!-- &#128452 is the unicode for a closed drawer --> <!-- source of unicodes http://xahlee.info/comp/unicode_office_icons.html -->
- <a href="/blog/learning-is-a-fractal-curve">Learning is a fractal curve</a>
-<br>
-<br>
-<br>
-<br>
-<hr>
-<br>
-<br>
-<h2>&#128452; &nbsp;Technology Slipbox</h2> My notes on development, CICD and infrastructure technologies and tools.
-<br>
-<br>
+  <hr />
+  <br /><br />
+  <h2>&#128240; &nbsp; Blog</h2>
+  <!-- &#128452 is the unicode for a closed drawer -->
+  <!-- source of unicodes http://xahlee.info/comp/unicode_office_icons.html -->
+  <a href="/blog/learning-is-a-fractal-curve">Learning is a fractal curve</a>
+  <br />
+  <br />
+  <br />
+  <br />
+  <hr />
+  <br />
+  <br />
+  <h2>&#128452; &nbsp;Technology Slipbox</h2>
+  My notes on development, CICD and infrastructure technologies and tools.
+  <br />
+  <br />
 
-<table class="slipbox">
+  <table class="slipbox">
     <tr class="drawer">
-         <td class="drawer_label">
-             CLOUD
-         </td>   
-         <td>     
-                      <a href="/infrastructure/cloud/linode">Linode</a> - Great as a sandbox to learn cloud basics
-<br>     
-                       GCP
-<br> 
-                       AWS
-<br>
-                       Azure
-<br>
-        
-         </td>
+      <td class="drawer_label"> CLOUD </td>
+      <td>
+        Linode- Great as a sandbox to learn cloud basics
+        <br />
+        GCP
+        <br />
+        AWS
+        <br />
+        Azure
+        <br />
+      </td>
     </tr>
 
     <tr class="drawer">
-         <td class="drawer_label">
-              KUBERNETES
-         </td>
-         <td>
-             RKE
-<br>
-             GKE
-<br>
-            
-         </td>
+      <td class="drawer_label"> KUBERNETES </td>
+      <td>
+        RKE
+        <br />
+        GKE
+        <br />
+      </td>
     </tr>
-     <tr>
-         <td class="drawer_label">
-	     CONTAINERISATION
-         </td>
-          <td>
-
-               <a href="/infrastructure/containerization/docker"> Docker</a>,  
-               <a href="/infrastructure/containerization/containerd"> Containerd</a>,  
-               <a href="/infrastructure/containerization/cri.o"> CRI.O</a>
-         </td>
-     </tr>
-
-     <tr>
-        <td class="drawer_label">
-            DATABASES
-        </td>
-        <td>
-            MongoDb
-<br>
-            Postgres
-<br>
-            MySQL
-<br>
-            Couchebase
-<br>
-     </tr>
-
-     <tr>
-         <td class="drawer_label">
-            TELEMETRY
-         </td>
-         <td>
-
-               <a href="/telemetry/apache_skywalking"> Apache Skywalking </a> 
-         </td>
-     </tr>
-
-     <tr>
-         <td class="drawer_label"> 
-            NETWORKING
-         </td> 
-         <td> 
-         
-              <a href="/infrastructure/networking/concepts/"> Networking concepts</a> 
-         
-         </td> 
+    <tr>
+      <td class="drawer_label"> CONTAINERISATION </td>
+      <td> Docker, Containerd, CRI.O </td>
     </tr>
 
     <tr>
-        <td class="drawer_label"> 
-             DEVELOPMENT
-        </td> 
-        <td>
-         
-               <a href="/development/go"> Go lang </a>,  
-               <a href="/webdev/svelte">Svelte + Sveltkit</a> &nbsp; A great framework for rapid web app development!
-        <td>
-   </tr>
+      <td class="drawer_label"> DATABASES </td>
+      <td>
+        MongoDb
+        <br />
+        Postgres
+        <br />
+        MySQL
+        <br />
+        Couchebase
+        <br />
+      </td></tr
+    >
 
-   <tr>
-       <td class="drawer_label">
-           PERFORMANCE ENGINEERING
-       </td>
-       <td>
-           K6 - load testing tool
-       </td>
-   </tr>
     <tr>
-        <td class="drawer_label">
-             EMERGING TECH
-        </td>
-        <td>
-            <a href="/emerging/web3">Web3</a> Decentralized block chain based development. Scary yet interesting.
-        <td>
+      <td class="drawer_label"> TELEMETRY </td>
+      <td> Apache Skywalking </td>
     </tr>
-</table>
 
-<br><br><br>
+    <tr>
+      <td class="drawer_label"> NETWORKING </td>
+      <td>
+        <a href="/infrastructure/networking/concepts/"> Networking concepts</a>
+      </td>
+    </tr>
 
+    <tr>
+      <td class="drawer_label"> DEVELOPMENT </td>
+      <td>
+        <a href="/development/go"> Go lang </a>,
+        <a href="/webdev/svelte">Svelte + Sveltkit</a> &nbsp; A great framework for
+        rapid web app development!
+      </td><td /></tr
+    >
 
-       
+    <tr>
+      <td class="drawer_label"> PERFORMANCE ENGINEERING </td>
+      <td> K6 - load testing tool </td>
+    </tr>
+    <tr>
+      <td class="drawer_label">HARDWARE</td>
+      <td> Cores </td>
+    </tr>
+    <tr>
+      <td class="drawer_label"> EMERGING TECH </td>
+      <td>
+        <a href="/emerging/web3">Web3</a> Decentralized block chain based development.
+        Scary yet interesting.
+      </td><td /></tr
+    >
+  </table>
 
-<br><br>
+  <br /><br /><br />
 
+  <br /><br />
 </main>
+
 <style>
 </style>
