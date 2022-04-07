198d94ca07f80534558ad69f522bd3ef9dbce9ef
vkrntgit <vikrantpayal@protonmail.com>
2022-04-07 18:49:48 +0530

    cleaned links

---
+++ build/_app/pages/infrastructure/hardware/cores.svelte-ea82df08.js
@@ -0,0 +1,15 @@
+import{S as g,i as _,s as b,e as r,t as d,c as a,a as v,h as m,d as p,g as y,F as n,H as f}from"../../../chunks/vendor-62c3b85b.js";function x(C){let e,s,i,c,h,u,l;return{c(){e=r("main"),s=r("h2"),i=d("Cores"),c=d(`
+  Deep dive into hardware.. Notes on Cores.
+  `),h=r("br"),u=r("br"),l=d(`
+  CPUs are the processing units in a computer. In the last century, a motherboard
+  used to have one processing unit (one CPU). With the dawn of the twenty first century,
+  most computers are at least dual core. Now (2022) we have personal use computers
+  with eight or sixteen core CPUs. High-end commercial use computers can have upwards
+  of 64 cores.`)},l(o){e=a(o,"MAIN",{});var t=v(e);s=a(t,"H2",{});var w=v(s);i=m(w,"Cores"),w.forEach(p),c=m(t,`
+  Deep dive into hardware.. Notes on Cores.
+  `),h=a(t,"BR",{}),u=a(t,"BR",{}),l=m(t,`
+  CPUs are the processing units in a computer. In the last century, a motherboard
+  used to have one processing unit (one CPU). With the dawn of the twenty first century,
+  most computers are at least dual core. Now (2022) we have personal use computers
+  with eight or sixteen core CPUs. High-end commercial use computers can have upwards
+  of 64 cores.`),t.forEach(p)},m(o,t){y(o,e,t),n(e,s),n(s,i),n(e,c),n(e,h),n(e,u),n(e,l)},p:f,i:f,o:f,d(o){o&&p(e)}}}class U extends g{constructor(e){super();_(this,e,null,x,b,{})}}export{U as default};
---
+++ src/routes/infrastructure/hardware/cores.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<main>
+  <h2>Cores</h2>
+  Deep dive into hardware.. Notes on Cores.
+  <br /><br />
+  CPUs are the processing units in a computer. In the last century, a motherboard
+  used to have one processing unit (one CPU). With the dawn of the twenty first century,
+  most computers are at least dual core. Now (2022) we have personal use computers
+  with eight or sixteen core CPUs. High-end commercial use computers can have upwards
+  of 64 cores.
+</main>
