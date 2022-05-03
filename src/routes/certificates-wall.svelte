c8422c76e32137ff38abc8365a1c09b3bcb87764
vkrntgit <vikrantpayal@protonmail.com>
2022-05-03 19:43:27 +0530

    kcna added

--- src/routes/certificates-wall.svelte
+++ src/routes/certificates-wall.svelte
@@ -10,7 +10,11 @@
 <a
   href="https://www.credly.com/badges/852a006c-2dcf-4c5c-bdb5-35bc3c039499"
   target="_blank">AWS Certified Cloud Practitioner</a
-><br />
+><br /><br />
+<a
+  href="https://www.credly.com/badges/3ea763c8-85cc-4844-b8ca-3a8613287b95"
+  target="_blank">Kubernetes Cloud Native Associate</a
+>
 
 <br /><br /><br />
 <h3>Leadership</h3>
